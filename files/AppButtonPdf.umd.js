393d653237775da3561d5bcb9f4d5a1f5411be1a
kouwa stephane <kksasteph888@gmail.com>
2022-12-16 23:45:39 +0100

    Maj

--- files/AppButtonPdf.umd.js
+++ files/AppButtonPdf.umd.js
@@ -2596,7 +2596,7 @@ if (typeof window !== 'undefined') {
 // EXTERNAL MODULE: external {"commonjs":"vue","commonjs2":"vue","root":"Vue"}
 var external_commonjs_vue_commonjs2_vue_root_Vue_ = __webpack_require__(7203);
 var external_commonjs_vue_commonjs2_vue_root_Vue_default = /*#__PURE__*/__webpack_require__.n(external_commonjs_vue_commonjs2_vue_root_Vue_);
-;// CONCATENATED MODULE: ./node_modules/babel-loader/lib/index.js??clonedRuleSet-82.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/ButtonGenerate.vue?vue&type=template&id=0814ea01&
+;// CONCATENATED MODULE: ./node_modules/babel-loader/lib/index.js??clonedRuleSet-82.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/ButtonGenerate.vue?vue&type=template&id=645faedd&
 var render = function render() {
   var _vm = this,
     _c = _vm._self._c;
@@ -15841,17 +15841,12 @@ E.API.PDFObject = function () {
         putOnlyUsedFonts: true,
         floatPrecision: 16 // or "smart", default is 16
       };
-
-      const promesse = new Promise(resolv => {
-        alert("init promise ");
-        resolv();
-      });
       //
       var doc = new E(options);
       // Convert HTML to PDF in JavaScript
       doc.html(html_data, {
         callback: function (doc) {
-          doc.save("output.pdf", promesse);
+          doc.save("output.pdf");
           self.openModel = false;
         },
         x: 0,
@@ -15862,10 +15857,10 @@ E.API.PDFObject = function () {
 });
 ;// CONCATENATED MODULE: ./src/components/ButtonGenerate.vue?vue&type=script&lang=js&
  /* harmony default export */ var components_ButtonGeneratevue_type_script_lang_js_ = (ButtonGeneratevue_type_script_lang_js_); 
-;// CONCATENATED MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-64.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-64.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-64.use[2]!./node_modules/sass-loader/dist/cjs.js??clonedRuleSet-64.use[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/ButtonGenerate.vue?vue&type=style&index=0&id=0814ea01&prod&lang=scss&
+;// CONCATENATED MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-64.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-64.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-64.use[2]!./node_modules/sass-loader/dist/cjs.js??clonedRuleSet-64.use[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/ButtonGenerate.vue?vue&type=style&index=0&id=645faedd&prod&lang=scss&
 // extracted by mini-css-extract-plugin
 
-;// CONCATENATED MODULE: ./src/components/ButtonGenerate.vue?vue&type=style&index=0&id=0814ea01&prod&lang=scss&
+;// CONCATENATED MODULE: ./src/components/ButtonGenerate.vue?vue&type=style&index=0&id=645faedd&prod&lang=scss&
 
 ;// CONCATENATED MODULE: ./node_modules/@vue/vue-loader-v15/lib/runtime/componentNormalizer.js
 /* globals __VUE_SSR_CONTEXT__ */
